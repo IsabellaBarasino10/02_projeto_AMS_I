40ec85e950337a263ecf68b38b46875ac6cab849
Aluno <aluno@IPIRANGA.FATEC>
2024-08-26 18:41:33 -0300

    Atividade 3

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes';
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use('/api', routes);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+ 
+  // Rota básica para a raiz
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+ 
+});
